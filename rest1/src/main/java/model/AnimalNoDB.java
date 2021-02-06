ff3d63ac635f4dfe7330ad07db1f0a0e18c3a889
SoerenDue <cph-sa343@cphbusiness.dk>
2021-02-06 15:14:26 +0100

    jpql demo and minor changes

---
+++ rest1/src/main/java/model/AnimalNoDB.java
@@ -0,0 +1,17 @@
+
+package model;
+
+/**
+ *
+ * @author zarpy
+ */
+public class AnimalNoDB {
+    private String type;
+    private String sound;
+    
+        public AnimalNoDB (String type, String sound){
+        this.type = type;
+        this.sound = sound;
+        
+    }
+}
